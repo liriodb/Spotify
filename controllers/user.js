20f848940d34a16386209ef384355b5dd5a0a256
liriodb <liriors2.0@gmail.com>
2018-08-06 21:15:36 +0200

    update user

--- controllers/user.js
+++ controllers/user.js
@@ -102,9 +102,33 @@ function loginUser(req, res){
     });
 }
 
+function updateUser(req, res){
+    let userId = req.params.id;
+    let update = req.body;
+
+    User.findByIdAndUpdate(userId, update, (err,userUpdated) =>{
+        if(err){
+            res.status(500).send({
+                message: 'Error al actualizar el usuario'
+            });
+        }else{
+            if(!userUpdated){
+                res.status(404).send({
+                    message: 'No se ha podido actualizar el usuario'
+                });
+            }else{
+                res.status(200).send({
+                    user: userUpdated
+                });
+            }
+        }
+    })
+}
+
 module.exports = {
     pruebas,
     saveUser,
-    loginUser
+    loginUser, 
+    updateUser
 };
 
--- routes/user.js
+++ routes/user.js
@@ -9,6 +9,7 @@ let md_auth = require('../middlewares/authenticated');
 api.get('/probando', md_auth.ensureAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
+api.put('/update-user/:id', md_auth.ensureAuth, UserController.updateUser);
 
 
 
