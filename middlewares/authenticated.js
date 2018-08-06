3de7b32184f1b0fd847856ecb7055906eddef5eb
liriodb <liriors2.0@gmail.com>
2018-08-06 20:28:05 +0200

    middleware

---
+++ middlewares/authenticated.js
@@ -0,0 +1,34 @@
+'use strict'
+
+let jwt = require('jwt-simple');
+let moment = require('moment');
+let secret = 'secret_password';
+
+exports.ensureAuth = function(req, res, next){
+    let payload;
+    if(!req.headers.authorization){
+        return res.status(403).send({
+            message: 'La peticion no tiene la cabecera de autenticacion'
+        });
+    }
+
+    let token = req.headers.authorization.replace(/['"]+/g, '');
+
+    try{
+        payload = jwt.decode(token, secret);
+        if(payload.exp <= moment().unix()){
+            return res.status(404).send({
+                message: 'El token ha expirado'
+            });
+        }
+    }catch(ex){
+        console.log(ex);
+        return res.status(404).send({
+            message: 'Token no valido'
+        });
+    }
+
+    req.user = payload;
+
+    next();
+}
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 'use strict'
 
-var express = require('express');
-var UserController = require('../controllers/user');
+let express = require('express');
+let UserController = require('../controllers/user');
 
-var api = express.Router();
+let api = express.Router();
+let md_auth = require('../middlewares/authenticated');
 
-api.get('/probando', UserController.pruebas);
+api.get('/probando', md_auth.ensureAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 
